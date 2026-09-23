66bb042649af844db43ea20b3f970bfad8e3e764
toddcauet <cauetmaletodd@gmail.com>
2026-09-23 17:34:54 +0200

    feat(barrett montgomery)
    * wip: barrett reduction, check ressources and add tests after
    
    * add barrett reduction and tests
    
    * add montgomery reduction and tests
    
    ---------
    
    Co-authored-by: todd <todd.cauet-male@univ-grenoble-alpes.fr>

---
+++ src/field.rs
@@ -0,0 +1,41 @@
+use crate::params::Q;
+
+const M: i32 = ((1 << 26) + (Q as i32 >> 1)) / Q as i32;
+const QINV: i16 = -3327;
+
+pub(crate) fn barrett_reduction(x: i16) -> i16 {
+    let t = ((x as i32 * M) + (1 << 25)) >> 26;
+    (x as i32 - t * Q as i32) as i16
+}
+
+pub(crate) fn montgomery_reduction(x: i32) -> i16 {
+    let m = (x as i16).wrapping_mul(QINV);
+    ((x - m as i32 * Q as i32) >> 16) as i16
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assert_barrett_reduction() {
+        for x in i16::MIN..=i16::MAX {
+            let res = barrett_reduction(x);
+            assert!((-(Q - 1) / 2..=(Q - 1) / 2).contains(&res));
+            assert_eq!(x.rem_euclid(Q), res.rem_euclid(Q));
+        }
+    }
+
+    #[test]
+    fn assert_montgomery_reduction() {
+        let bound: i32 = Q as i32 * (1 << 15);
+        for x in (-bound + 1)..bound {
+            let res = montgomery_reduction(x);
+            assert!((-(Q - 1)..=(Q - 1)).contains(&res), "{x} = {res}");
+            assert_eq!(
+                (res as i64 * 65536).rem_euclid(Q as i64),
+                (x as i64).rem_euclid(Q as i64)
+            );
+        }
+    }
+}
