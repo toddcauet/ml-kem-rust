8cbf0f141bc5c2d58b35aad28f902243564fe41e
toddcauet <cauetmaletodd@gmail.com>
2026-09-24 18:52:47 +0200

    feat(field): add branchless add and sub
    * add caddq and tests
    
    * add csubq and tests
    
    * add add and sub with tests

--- src/field.rs
+++ src/field.rs
@@ -13,6 +13,23 @@ pub(crate) fn montgomery_reduction(x: i32) -> i16 {
     ((x - m as i32 * Q as i32) >> 16) as i16
 }
 
+pub(crate) fn caddq(x: i16) -> i16 {
+    let mask: i16 = (x >> 15) & Q;
+    x + mask
+}
+
+pub(crate) fn csubq(x: i16) -> i16 {
+    caddq(x - Q)
+}
+
+pub(crate) fn add(x: i16, y: i16) -> i16 {
+    csubq(x + y)
+}
+
+pub(crate) fn sub(x: i16, y: i16) -> i16 {
+    caddq(x - y)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,6 +53,44 @@ mod tests {
                 (res as i64 * 65536).rem_euclid(Q as i64),
                 (x as i64).rem_euclid(Q as i64)
             );
+        }
+    }
+
+    #[test]
+    fn assert_caddq() {
+        for x in (-Q)..Q {
+            let res = caddq(x);
+            assert!((0..Q).contains(&res), "{x} = {res}");
+            assert_eq!(x.rem_euclid(Q), res.rem_euclid(Q));
+        }
+    }
+
+    #[test]
+    fn assert_csubq() {
+        for x in 0..2 * Q {
+            let res = csubq(x);
+            assert!((0..Q).contains(&res), "{x} = {res}");
+            assert_eq!(x.rem_euclid(Q), res.rem_euclid(Q));
+        }
+    }
+
+    #[test]
+    fn assert_add() {
+        for x in 0..Q {
+            for y in 0..Q {
+                let res = add(x, y);
+                assert_eq!(res, (x + y).rem_euclid(Q));
+            }
+        }
+    }
+
+    #[test]
+    fn assert_sub() {
+        for x in 0..Q {
+            for y in 0..Q {
+                let res = sub(x, y);
+                assert_eq!(res, (x - y).rem_euclid(Q));
+            }
         }
     }
 }
